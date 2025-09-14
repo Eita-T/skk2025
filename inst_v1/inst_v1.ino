e8c7667d8b29b79d883b6b379ce653318aaee267
eita-w-dynabook <eitatsuguta@gmail.com>
2025-09-14 15:09:15 +0900

    メトロノーム

--- inst_v1/inst_v1.ino
+++ inst_v1/inst_v1.ino
@@ -1,10 +1,16 @@
 #include <Modulino.h>
+#include "Arduino_LED_Matrix.h"
+#include "FspTimer.h"
+
 ModulinoButtons buttons;
+ArduinoLEDMatrix matrix;
+FspTimer _timer;
 
 const int speakerPin = A0;
 const int pascalPin  = A2;
 const int joyY       = A4; // Y軸（半音上げ下げ）
 const int joyX       = A5; // X軸（オクターブ上下）
+const int joyZ       = 3;    // ジョイスティックZ軸(ボタン)
 
 int chromaticFreq[] = {
   261, 277, 293, 311, 329, 349,
@@ -14,19 +20,56 @@ int chromaticFreq[] = {
 int naturalIndex[9] = {0, 0, 2, 4, 5, 7, 9, 11, 12};
 
 int pascalState = 0;
-bool isplusflag = 0;
+bool isplusflag = 0, isMatrixOn = 0, timerFlag = 0;
+
+const uint32_t on[] = {
+    0xFFFFFFFF,
+    0xFFFFFFFF,
+    0xFFFFFFFF
+};
+const uint32_t off[] = {
+    0x00000000,
+    0x00000000,
+    0x00000000
+};
+
+void callbackfunc(timer_callback_args_t *arg) {
+  isMatrixOn = !isMatrixOn;
+}
+
 
 void setup() {
   Serial.begin(115200);
   Modulino.begin();
   buttons.begin();
+  matrix.begin();
   pinMode(pascalPin, INPUT);
-  pinMode(joyY, INPUT);
   pinMode(joyX, INPUT);
+  pinMode(joyY, INPUT);
+  pinMode(joyZ, INPUT);
+
+  uint8_t type;
+  int8_t ch = FspTimer::get_available_timer(type);
+  if (ch < 0) {
+    return;
+  }
+  _timer.begin(TIMER_MODE_PERIODIC, type, ch, 1.0f, 50.0f, callbackfunc, nullptr);
+  _timer.setup_overflow_irq();
+  _timer.open();
+  _timer.start();
 }
 
 void loop() {
   static int iicbuttonState = 0, buttonState = 0;
+  int joyZState = digitalRead(joyZ);
+  
+  if(isMatrixOn) {
+    Serial.println("on");
+    matrix.loadFrame(on);
+  }else {
+    Serial.println("off");
+    matrix.loadFrame(off);
+  }
 
   if (buttons.update()) {
     if (buttons.isPressed(0) && iicbuttonState/4 == 0) iicbuttonState += 4;
